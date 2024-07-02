15a5cecf8628cb4cc8f85b89efced6f65d482337
Lakshayraj04 <i7655099@gmail.com>
2024-07-02 22:18:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var Typed = new Typed(".text", { 
+	strings: ["IT Student","Learner","Frontend Developer"],
+	typeSpeed:200,
+	backspeed:100,
+	backdelay:1000,
+	loop:true
+    });
+
+
+var Typed = new Typed(".text1", { 
+	strings: ["frontend developer","youtuber","fullstack developer"],
+	typeSpeed:100,
+	backspeed:100,
+	backdelay:1000,
+	loop:true
+    });
+
+
+
+
+
+		/*<h3>And I'm a <span class="text"> </span></h3>*/
